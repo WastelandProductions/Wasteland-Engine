2ef9547e53849ea2fe63a1cacbec7ce9a5db0911
rtoue <rtoue@NICKPC>
2026-05-07 10:39:53 -0400

    started adding properties panel to scene objects

--- DemonCore-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ DemonCore-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -4,6 +4,8 @@
 
 #include <Wasteland/Scene/Components.h>
 
+#include <glm/gtc/type_ptr.hpp>
+
 namespace Wasteland {
 
 	SceneHierarchyPanel::SceneHierarchyPanel(const Ref<Scene>& context)
@@ -25,7 +27,16 @@ namespace Wasteland {
 				Entity entity{ entityID, m_Context.get() };
 				DrawEntityNode(entity);
 			});
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
 			
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
+
 		ImGui::End();
 	}
 
@@ -48,6 +59,33 @@ namespace Wasteland {
 				bool opened = ImGui::TreeNodeEx((void*)9817239, flags, tag.c_str());
 				if (opened)
 					ImGui::TreePop();
+				ImGui::TreePop();
+			}
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer))) 
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
 				ImGui::TreePop();
 			}
 		}
